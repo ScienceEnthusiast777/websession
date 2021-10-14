c0788c72161db8f4f7130891aacd9abc97330e40
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-10-14 16:35:55 +0200

    install react-cookie package and set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./Main";
+import { useCookies } from "react-cookies";
 
 function App() {
+  const [cookies,setCookie] = useCookies(["session"])
+  function createCookie(){
+    let time = new Date()
+    time.setTime(time.getTime() + 30*60*1000)
+    setCookie("session", "",{path:"/",})
+  }
   return (
     <div className="App">
       <Main />
